6b3f79f87cf406aaa3d3beaf8ce8d29630287498
Huy Tran <quanghuy.hut.fami@gmail.com>
2023-09-19 21:01:28 +0200

    Fix spotless issue

--- src/main/java/com/snaacker/jpa/persistent/Author.java
+++ src/main/java/com/snaacker/jpa/persistent/Author.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
